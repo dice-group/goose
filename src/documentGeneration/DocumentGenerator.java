9bd9b3dbb7b8860ad8fef5f79f178f43d9137eb4
Lars Werneke <lwerneke@pc-wehr-pool07.cs.uni-paderborn.de>
2017-11-15 16:18:05 +0100

    Added basic source files with todo information

---
+++ src/documentGeneration/DocumentGenerator.java
@@ -0,0 +1,8 @@
+package documentGeneration;
+
+//https://github.com/SmartDataAnalytics/SemWeb2NL
+//https://github.com/dice-group/AGDISTIS/tree/master/src/main/java/org/aksw/agdistis/util
+public interface DocumentGenerator {
+
+	public void generate();
+}
